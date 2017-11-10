fd905904f4bae5a9aae32172649d171ed8c01cbb
Jorge <jromussi@gmail.com>
2017-11-10 01:10:21 -0300

    Devise avanzado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token  
+  before_action :authenticate_user!
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if current_user then
+    	'application'
+    else
+		'login'
+    end
+  end
 end
